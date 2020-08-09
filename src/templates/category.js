0a9f25eb22ca620c7ee1550d70bd92f48f9f8b11
Sion Wilks-Boguszewicz <58896718+sioncamara@users.noreply.github.com>
2020-08-08 22:52:18 -0500

    First commit

---
+++ src/templates/category.js
@@ -0,0 +1,46 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import ArticlesComponent from "../components/articles"
+import Layout from "../components/layout"
+
+export const query = graphql`
+  query Category($id: Int!) {
+    articles: allStrapiArticle(filter: { category: { id: { eq: $id } } }) {
+      edges {
+        node {
+          strapiId
+          title
+          category {
+            name
+          }
+          image {
+            name
+          }
+        }
+      }
+    }
+    category: strapiCategory(strapiId: { eq: $id }) {
+      name
+    }
+  }
+`
+
+const Category = ({ data }) => {
+  const articles = data.articles.edges
+  const category = data.category.name
+  console.log("articles from component", articles);
+
+  return (
+    <Layout>
+      <div className="uk-section">
+        <div className="uk-container uk-container-large">
+          <h1>{category}</h1>
+          <ArticlesComponent articles={articles} />
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export default Category
